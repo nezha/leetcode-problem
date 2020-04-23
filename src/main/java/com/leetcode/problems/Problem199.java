2fdbc20c02f54e0798f42941caae8a04068659eb
nezha <1061243497@qq.com>
2020-04-23 22:58:42 +0800

    #leetcode-199

---
+++ src/main/java/com/leetcode/problems/Problem199.java
@@ -0,0 +1,72 @@
+package com.leetcode.problems;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author: nezha <br>
+ * @Title: Problem199 <br>
+ * @ProjectName: leetcode-problem <br>
+ * @Description: xxxxx <br>
+ * @Date: 2020/4/23 10:33 PM <br>
+ * <p>
+ * 给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [1,2,3,null,5,null,4]
+ * 输出: [1, 3, 4]
+ * 解释:
+ * <p>
+ * 1            <---
+ * /   \
+ * 2     3         <---
+ * \     \
+ * 5     4       <---
+ *
+ * 题解：
+ * 1.通过BFS，广度优先
+ */
+public class Problem199 {
+    /**
+     * Definition for a binary tree node.
+     */
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                //将当前层的最后一个节点放入结果列表
+                if (i == size - 1) {
+                    res.add(node.val);
+                }
+            }
+        }
+        return res;
+    }
+
+}
